b81dedccd32f82c749a5e46cc88e46616e1edff5
Informatica <informatica@alumno.local>
2024-01-19 11:53:26 -0600

    Promesas 1.1

---
+++ promesas/assert/main.js
@@ -0,0 +1,36 @@
+
+function enviarDatos(){
+    const nombre = document.getElementById('nombre').value;
+    enviarDatosAlServidor(nombre)
+        .then(mensajeAgradecimiento)
+        .catch(mensajeError);
+}
+
+function enviarDatosAlServidor(nombre){
+    return new Promise((resolve,reject) => {
+        
+        setTimeout(() => {
+            const exito = (nombre.toLowerCase() !== 'error');
+
+            if(exito){
+                resolve('Bienvenido al Sistema');
+            }else{
+                reject('Acceso Denegado');
+            }
+
+        }, 2000);
+   
+    });
+};
+
+function mensajeAgradecimiento(mensaje){
+    const mensajeElement = document.getElementById('mensaje');
+    mensajeElement.textContent = mensaje;
+    mensajeElement.classList.remove('oculto');
+}
+
+function mensajeError (error){
+    const mensajeElement = document.getElementById('mensaje');
+    mensajeElement.textContent = error;
+    mensajeElement.classList.remove('oculto');
+}
